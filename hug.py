90dd7b2dc56ae34c83c5027d8bcd3478a2101639
Renan Tashiro <renantashiro@hotmail.com>
2016-12-12 19:16:59 -0200

    ADD LICENSE AND CHANGE THE FILE NAME

---
+++ hug.py
@@ -0,0 +1,78 @@
+'''
+Copyright (c) 2016-2017 Plug-and-share
+All rights reserved.
+The license below extends only to copyright in the software and shall
+not be construed as granting a license to any other intellectual
+property including but not limited to intellectual property relating
+to a hardware implementation of the functionality of the software
+licensed hereunder.  You may use the software subject to the license
+terms below provided that you ensure that this notice is replicated
+unmodified and in its entirety in all distributions of the software,
+modified or unmodified, in source code or in binary form.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met: redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer;
+redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution;
+neither the name of the copyright holders nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+VERSION: v0.001
+DATE: 12/01/2016
+AUTHORS: Canabarro, Dias, Tashiro
+PYTHON_VERSION: v3
+'''
+import optparse							
+
+def command():
+	'''
+	Show the available commands and parse them
+	'''
+	usage = 'Cupid\nVersion 0.0.0\nBy The Cupid Team: https://github.com/plug-and-share/\nUsing python >=3'
+	parser = optparse.OptionParser(usage)
+	parser.add_option('-r', '--run', action='store_true', 
+			help='Start running pine')
+	parser.add_option('-p', '--pause', action='store_true', 
+			help='Pause running pine')
+	parser.add_option('-s', '--stop', action='store_true', 
+			help='Shutdown pine')
+	parser.add_option('-c', '--collaborate', action='store_true', 
+			help='Allocate the machine resource to some application')
+	parser.add_option('-R', '--resource', action='store_true', 
+			help='Manage the resource usage of the machine')
+	parser.add_option('-C', '--config', action='store_true',
+			help='Show the actual pine configuration')
+	parser.add_option('-v', '--version', action='store_true',
+			help='Cupid version number')
+	return parser.parse_args()
+
+if __name__ == '__main__':
+	opt, args = command()
+	if opt.run:
+		print('run')
+	elif opt.pause:
+		print('pause')
+	elif opt.stop:
+		print('stop')
+	elif opt.collaborate:
+		print('collaborate')
+	elif opt.resource:
+		print('resource')
+	elif opt.config:
+		print('config')
+	elif opt.version:
+		print('Cupid\nVersion 0.0.0\nBy The Cupid Team: https://github.com/plug-and-share/pine\nUsing python >=3')
